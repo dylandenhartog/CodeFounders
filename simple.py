190d8030ed53857bd71fc293326205ca0e320adf
dylandenhartog <dylan.den.hartog@hotmail.com>
2023-04-07 16:13:17 +0200

    First commit

---
+++ simple.py
@@ -0,0 +1,16 @@
+# Comment
+
+print("Hello world!")
+
+def func1():
+    print("Test")
+    print("Test1")
+
+def func2(tekst):
+    print("De tekst is " + tekst)
+    i = 123
+    print("i = " + str(i))
+
+func1();
+func2("Martijn");
+
